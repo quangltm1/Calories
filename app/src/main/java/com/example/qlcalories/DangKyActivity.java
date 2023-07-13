5d3c82e4965444943a29fd9cd31e6e80bbcc955a
unknown <doanduongkhang0107@gmail.com>
2023-07-13 08:57:59 +0700

    dang ky

---
+++ app/src/main/java/com/example/qlcalories/DangKyActivity.java
@@ -0,0 +1,13 @@
+package com.example.qlcalories;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DangKyActivity extends AppCompatActivity {
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dangky);
+    }
+}
